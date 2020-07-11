87de074d7511cda8b816076de9f35741b8df8240
amirynia <amirynia@yahoo.com>
2020-07-11 18:59:08 -0400

    optimization net fix

--- optimizationrunDE.py
+++ optimizationrunDE.py
@@ -11,7 +11,7 @@ import ieee802154
 from node import Node
 import LEACH 
 import clusteringKMEANS as KMEANS
-
+import pickle
 
 def run(x):
 
@@ -28,15 +28,23 @@ def run(x):
      startstatic = False
      if(startstatic):
           import network as initialieee802154
+          net1 = initialieee802154.net1
+          env = initialieee802154.env
      else:
-          import networkfrommaker as initialieee802154
+          positions =[]
+          positions = pickle.load(open("report/pos.pickle", "rb"))
+          env = simpy.Environment()
+          net1 = ieee802154.Net(env)
+          net1.add_node(Node(0, env, 4, (config.xsize)/2, (config.ysize)/2, node_type='B' ,power_type=0,ieee802154 =net1))
+          for i in range(len(positions)):
+               if (i >0):
+                    x = positions[i][0]
+                    y = config.ysize - positions[i][1]
+                    #print("p = x:",positions[i][0],positions[i][1],i)
+                    net1.add_node(Node(i,env,2000,x,y,ieee802154=net1))
+     
 
-     # create ieee802154
-     net1 = initialieee802154.net1
-     env = initialieee802154.env
-    
-#     env = simpy.Environment()
-#     net1 = ieee802154.Net(env)
+
 
 
 #     net1.add_node(Node(0, env, 4, (config.xsize)/2, (config.ysize)/2, node_type='B' ,power_type=0,ieee802154 =net1))
